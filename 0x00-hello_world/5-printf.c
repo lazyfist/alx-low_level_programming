07f89416f1f624e97e420f919be13e71c2b71c90
Elvis Mwangi <githinjimelvis@gmail.com>
2022-09-08 13:44:17 -0700

    print correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,11 @@
 
 /**
  * main - acts as an entry point
+ *return - always 0
  */
 int main(void)
 {
-printf("With proper grammar, but the outcome is a piece of art,\n");
-return(0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
 
