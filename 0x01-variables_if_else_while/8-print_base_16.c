d059204d4f1c2a56585c92fca07c579f747c25cc
Elvis Mwangi <githinjimelvis@gmail.com>
2022-09-09 15:06:12 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base_16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+	return (0);
+}
