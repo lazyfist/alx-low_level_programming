50e9060f9728ebfcb6467a37410877bba78ea4e8
Elvis Mwangi <githinjimelvis@gmail.com>
2022-09-09 15:22:14 -0700

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num <=57)
+	{
+		putchar(num);
+
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
