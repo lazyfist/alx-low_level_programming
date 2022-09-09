91fc76f82f60175eda48012f329fb0681915fe08
Elvis Mwangi <githinjimelvis@gmail.com>
2022-09-09 14:24:38 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char num;
+
+	for (num = 48; num <= 57; num++)
+	{
+	putchar(num);
+	}
+
+	putchar('\n');
+	return (0);
+}
